9b40c2d1840e7bcbdba21132070fdd0fa7acd8d0
Jonathan Torres <jonathan.torresmo@amigo.edu.com>
2026-02-21 09:46:41 -0500

    Creacion del componente Formulario.js y modificacion en el archivo Apps.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Formulario from './src/components/Formulario';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Este es el home de la app</Text>
+      <Formulario />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Formulario.js
@@ -0,0 +1,8 @@
+import { Text, View } from 'react-native';
+export default function Formulario() {
+    return (
+        <View style={styles.container}>
+            <Text>Este es el formulario de la app</Text>
+        </View>
+    );
+}
