9fe97439d10b84d729a388acbee85af3357c491f
Jonathan Torres <jonathan.torresmo@amigo.edu.com>
2026-03-21 08:03:04 -0500

    cambios en el archivo App

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import AppNavigator from './navigation/AppNavigator';
-import AppProvider from './';
+import AppProvider from './navigation/AppProvider';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
